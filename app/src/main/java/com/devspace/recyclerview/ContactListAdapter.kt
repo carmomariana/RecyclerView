fbcf375fcbf6972a51d5e61223d2902e49659a9f
carmomariana <mariana18oc@gmail.com>
2024-05-07 08:43:58 -0300

    Implementando Adapter

--- app/src/main/java/com/devspace/recyclerview/ContactListAdapter.kt
+++ app/src/main/java/com/devspace/recyclerview/ContactListAdapter.kt
@@ -1,7 +1,10 @@
 package com.devspace.recyclerview
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
@@ -9,16 +12,30 @@ import androidx.recyclerview.widget.RecyclerView
 // Adaptação entre o data class e o item_list layout
 class ContactListAdapter:
     ListAdapter<Contact, ContactListAdapter.ContactViewHolder>(ContactDiffUtils ()) {
+
+        //Criar um view holder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_list, parent, false)
+            return ContactViewHolder(view)
     }
+
+    //Bind - atrelar o dado com a UI views
     override fun onBindViewHolder(holder: ContactViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val contact = getItem(position)
+        holder.bind(contact)
     }
 
-// View holder = view que segura os dados
+    // View holder = view que segura os dados
     class ContactViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val tvName = view.findViewById<TextView>(R.id.tv_name)
+        private val tvPhone = view.findViewById<TextView>(R.id.tv_phone)
+        private val image = view.findViewById<ImageView>(R.id.image)
+        fun bind(contact: Contact){
+            tvName.text = contact.name
+            tvPhone.text = contact.phone
+            image.setImageResource(contact.icon)
 
+        }
     }
 
     //Compara a diferença quando a nossa lista é atualizada
